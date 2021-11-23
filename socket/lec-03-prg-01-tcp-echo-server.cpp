195b954cbebd341e2fe5a10bd0e9570ca3c08759
tjswodud <tjswodud85@gmail.com>
2021-11-24 02:54:10 +0900

    [NEW] lec-03-prg-01-tcp-echo-server.cpp & lec-03-prg-02-tcp-echo-client.cpp

---
+++ socket/lec-03-prg-01-tcp-echo-server.cpp
@@ -0,0 +1,72 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+int main(int argc, char* argv[])
+{
+    std::string HOST = "127.0.0.1";
+    unsigned int PORT = 65456;
+    std::string PORT_S(std::to_string(PORT));
+
+    int clientSocket, c;
+    int opt = 1;
+    struct sockaddr_in server, client;
+
+    char buffer[1024], recvData[1024];
+    char* recvMsg;
+
+    std::cout << "> echo-server is activated" << std::endl;
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+    
+    if (bind(serverSocket, (struct sockaddr*)& server, sizeof(server)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(serverSocket, 3) < 0)
+    {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    clientSocket = accept(serverSocket, (struct sockaddr*)& client, (socklen_t*)& c);
+    if (clientSocket < 0)
+    {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "> client connected by IP address " << HOST << " with Port number " << PORT_S << std::endl;
+    while (true)
+    {
+        recv(clientSocket, buffer, 1024, 0);
+        strcpy(recvData, buffer);
+        send(clientSocket, recvData, strlen(recvData) + 1, 0);
+        std::cout << "> echoed: " << buffer << std::endl;
+        if (strcmp(buffer, "quit") == 0)
+        {
+            std::cout << "> echo-server is de-activated" << std::endl;
+            break;
+        }
+    }
+}
---
+++ socket/lec-03-prg-02-tcp-echo-client.cpp
@@ -0,0 +1,50 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+int main(int argc, char* argv[])
+{
+    std::string HOST = "127.0.0.1";
+    unsigned int PORT = 65456;
+
+    int client_sock, c;
+    struct sockaddr_in server, client;
+
+    char* sendMsg;
+    ssize_t recvData;
+    char sendBuffer[2048];
+    char recvBuffer[2048];
+
+    std::cout << "> echo-client is activated" << std::endl;
+
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket < 0)
+    {
+        std::cout << "\n Socket creation error \n";
+        return -1;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+
+    if (connect(clientSocket, (struct sockaddr*)& server, sizeof(server)) < 0)
+    {
+        std::cout << "\nConnection Failed \n";
+        return -1;
+    }
+    
+    while (true)
+    {
+        std::cout << "> ";
+        std::cin >> sendBuffer;
+        send(clientSocket, sendBuffer, strlen(sendBuffer) + 1, 0);
+        recv(clientSocket, recvBuffer, 1024, 0);
+        std::cout << "> received: " << recvBuffer << std::endl;
+        if (strcmp(sendBuffer, "quit") == 0)
+            break;
+    }
+}
